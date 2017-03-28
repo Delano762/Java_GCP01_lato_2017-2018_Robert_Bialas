5f81f007b3ef2acd95d5e066e52ba7bee4e7cd8d
Delano762 <Delano762@gmail.com>
2017-03-28 19:28:16 +0200

    28.03.2017 JavaFX
    Dodanie JavaFX: Przycisk Hello World uruchamiający Crawlera

--- src/com/example/Main.java
+++ src/com/example/Main.java
@@ -4,34 +4,63 @@ import com.example.log.ConsoleLogger;
 import com.example.log.Logger;
 import com.example.log.MailLogger;
 import javafx.application.Application;
-
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 import java.net.URL;
 public class Main extends Application{
+    static String arguments[];
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
 
-    public static void main(String[] args) throws Exception{
-        final Logger[] loggers = {new ConsoleLogger(),new MailLogger(args[1],args[2],args[3])};
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+                final Logger[] loggers = {new ConsoleLogger(),new MailLogger(arguments[1],arguments[2],arguments[3])};
 
-        new Thread(()->{
-            try{
-                final Crawler crawler = new Crawler(new URL("http://home.agh.edu.pl/~ggorecki/IS_Java/students.txt"),args[0]);
-                crawler.addIterationStartedListener((crawlerEvent -> System.out.println("Iteration"+crawlerEvent.getIteration())));
-                crawler.addStudentAddedListener((crawlerEvent -> {
-                    for(Logger logger : loggers){
-                        logger.log("Added: "+crawlerEvent.getStudent().getFirstName()+" " + crawlerEvent.getStudent().getLastName(),crawlerEvent.getStudent());
+                new Thread(()->{
+                    try{
+                        final Crawler crawler = new Crawler(new URL("http://home.agh.edu.pl/~ggorecki/IS_Java/students.txt"),arguments[0]);
+                        crawler.addIterationStartedListener((crawlerEvent -> System.out.println("Iteration"+crawlerEvent.getIteration())));
+                        crawler.addStudentAddedListener((crawlerEvent -> {
+                            for(Logger logger : loggers){
+                                logger.log("Added: "+crawlerEvent.getStudent().getFirstName()+" " + crawlerEvent.getStudent().getLastName(),crawlerEvent.getStudent());
+                            }
+                        }));
+                        crawler.addIterationFinishedListeners(new CrawlerListener() {
+                            @Override
+                            public void actionPerformed(CrawlerEvent crawlerEvent) {
+
+                            }
+                        });
+                        crawler.addStudentNoChangeListener((crawlerEvent -> System.out.println(crawlerEvent.getStudent().toString()+ ":"+crawlerEvent.getType())));
+                        crawler.run();
+                    }catch (Exception e){
+                        e.printStackTrace();;
+                        System.out.println("Error");
                     }
-                }));
-                crawler.addIterationFinishedListeners(new CrawlerListener() {
-                    @Override
-                    public void actionPerformed(CrawlerEvent crawlerEvent) {
-
-                    }
-                });
-                crawler.addStudentNoChangeListener((crawlerEvent -> System.out.println(crawlerEvent.getStudent().toString()+ ":"+crawlerEvent.getType())));
-                crawler.run();
-            }catch (Exception e){
-                e.printStackTrace();;
-                System.out.println("Error");
+                }).start();
             }
-        }).start();
+        });
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+
+        Scene scene = new Scene(root, 300, 250);
+
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    public static void main(String[] args) throws Exception{
+
+        arguments=args;
+        launch(args);
     }
 }
